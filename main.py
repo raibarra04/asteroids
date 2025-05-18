5248d8db65363d921d4157d721a9e3dc39395f80
raibarra04 <ribarra764@gmail.com>
2025-05-18 13:46:28 -0700

    circleshape class to draw out asteroid and player shapes, added player class, add constants, added functionality to allow player to move left and right

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import pygame
 import os
 from constants import *
+from player import Player
 
 os.environ['SDL_AUDIODRIVER'] = 'dummy'
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
-        pygame.display.flip()
+        player.update(dt)
 
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
