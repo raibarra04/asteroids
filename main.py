f8d1ffa6629ce1fd2c2d3cf304bde861c80bb541
raibarra04 <ribarra764@gmail.com>
2025-05-18 14:23:04 -0700

    added pygame groups to modularize multiple objects in the game

--- main.py
+++ main.py
@@ -9,18 +9,28 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        player.update(dt)
-
+        
+        updatable.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
