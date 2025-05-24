f679017e7a0f8a5250483a7fccd5ec1048e04f53
raibarra04 <ribarra764@gmail.com>
2025-05-23 23:17:02 -0700

    Added game over screen, and ability to replay from death

--- main.py
+++ main.py
@@ -11,6 +11,26 @@ from shot import Shot
 
 os.environ['SDL_AUDIODRIVER'] = 'dummy'
 
+def game_over(screen):
+    font = pygame.font.Font(None, 48)
+    text = font.render('Game Over!', True, 'red')
+    text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+    screen.blit(text, text_rect)
+    pygame.display.flip()
+
+    while True:
+        for event in pygame.event.get():
+            keys = pygame.key.get_pressed()
+            
+            if keys[pygame.K_y]:
+                main()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.quit()
+                sys.exit()
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -40,8 +60,9 @@ def main():
         
         for asteroid in asteroids:
             if player.collision(asteroid):
-                print("Game Over!")
-                sys.exit()
+                game_over(screen)
+                #print("Game Over!")
+                #sys.exit()
   
             for shot in shots:
                 if asteroid.collision(shot):
